ca82c11de5d62d6cfc3b4219dac6cfd01bf6639c
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-03-01 11:40:28 +0530

    added AI-Help button

--- content.js
+++ content.js
@@ -1,7 +1,33 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const aiHelpImgURL = chrome.runtime.getURL("assets/ai-help-white.png");
+
+addAIHelpButton();
+
+const observer = new MutationObserver(() => {
+  addAIHelpButton();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+function addAIHelpButton() {
+  if (!onProblemsPage() || document.getElementById("ai-help-button")) return;
+
+  const aiHelpButton = document.createElement("img");
+  aiHelpButton.id = "ai-help-button";
+  aiHelpButton.src = aiHelpImgURL;
+  aiHelpButton.style.height = "40px";
+  aiHelpButton.style.width = "40px";
+  aiHelpButton.style.cursor = "pointer";
+
+  const askDoubtButton = document.getElementsByClassName(
+    "coding_ask_doubt_button__FjwXJ"
+  )[0];
+  askDoubtButton.parentNode.insertAdjacentElement("afterend", aiHelpButton);
+
+  aiHelpButton.addEventListener("click", function () {
+    console.log("Button clicked");
+  });
+}
+
+function onProblemsPage() {
+  return window.location.pathname.startsWith("/problems/");
+}
